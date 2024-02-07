44e216fdd557136f51757345026ae9ea5665c7c0
Timeea Samoila <timeeasamoila12@gmail.com>
2024-02-07 12:55:50 +0100

    changes

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -3,4 +3,14 @@ class Pet < ApplicationRecord
   belongs_to :shelter
 
   has_many :orders
+  has_one_attached :picture
+  def picture_url
+    if type == 'cat'
+      'cat.png'
+    elsif type == 'dog'
+      'dog.png'
+    else
+      'puppy-test'
+    end
+  end
 end
