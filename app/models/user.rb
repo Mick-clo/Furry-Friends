6f94be79e9b6833fc8d376b407ac69c51a14d910
Timeea Samoila <timeeasamoila12@gmail.com>
2024-02-12 17:29:39 +0100

    fking hell

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   require_relative 'pet'
   has_many :messages, dependent: :destroy
   has_many :pets, dependent: :destroy
-
+  has_one_attached :avatar
   has_many :orders, through: :pets
   validates :username, presence: true
   # validates :satisfaction, presence: true, numericality: { only_integer: true }
--- config/routes.rb
+++ config/routes.rb
@@ -32,4 +32,5 @@ Rails.application.routes.draw do
       post 'care'
     end
   end
+  resources :users, only: [:update]
 end
