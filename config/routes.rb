6bee819cf94a5c514731abc901d46852495229f9
michele comande <comande.michele@gmail.com>
2024-02-13 11:34:28 +0000

    forgot to create a new branch

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   require "sidekiq/web"
   mount Sidekiq::Web => '/sidekiq'
-  # mount StripeEvent::Engine, at: '/stripe-webhooks'
+  mount StripeEvent::Engine, at: '/stripe-webhooks'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
